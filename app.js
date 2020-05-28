5ecd38fca3f03b515b7297ee1bfc83b60c242f2a
jripke74 <jripke74@gmail.com>
2020-05-28 13:05:43 -0700

    other way to add properties

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-// class AgedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
 
-// class Person extends AgedPerson {
-//   name = 'Max';
+class Person {
+  name = 'Max';
 
-//   constructor() {
-//     super();
-//     this.age = 30;
-//   }
+  constructor() {
+    this.age = 30;
+  }
 
-//   greet() {
-//     console.log(`Hi, I am ${this.age} and my name is ${this.name}.`);
-//   }
-// }
-
-function Person() {
-  this.age = 30;
-  this.name = 'Jeff';
-  this.greet = function() {
+  greet = () => {
     console.log(`Hi, I am ${this.age} and my name is ${this.name}.`);
-  };
+  }
+
+  // greet() {
+  //   console.log(`Hi, I am ${this.age} and my name is ${this.name}.`);
+  // }
 }
 
-Person.describe = function() {
-  console.log('Creating persons...');
-}
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Jeff';
+// }
+
+// Person.prototype.greet = function() {
+//   console.log(`Hi, I am ${this.age} and my name is ${this.name}.`);
+// };
+
+// Person.describe = function() {
+//   console.log('Creating persons...');
+// }
 
 // Person.prototype = {
 //   printAge() {
@@ -35,16 +39,63 @@ Person.describe = function() {
 //   }
 // }
 
-Person.prototype.printAge = function() {
-  console.log(this.age);
+// Person.prototype.printAge = function() {
+//   console.log(this.age);
+// };
+
+// console.dir(Person);
+
+// const person = new Person();
+// person.greet();
+// person.printAge();
+// console.log(person)
+// console.log(person.toString());
+// const p2 = new person.__proto__.constructor();
+// console.dir(Object.prototype.__proto__);
+
+// const p = new Person();
+// const p2 = new Person();
+// console.log(p.__proto__ === p2.__proto__);
+// console.log(p);
+
+// const button = document.getElementById('btn');
+// button.addEventListener('click', p.greet)
+
+const course = { // new Object()
+  title: 'JavaScript - The Complete Guide',
+  rating: 5
 };
 
-console.dir(Person);
+// console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function() {
+    console.log(`${this.rating}/5`)
+  }
+}, {
+  name: {
+    configurable: false,
+    enumerable: true,
+    value: 'Jeff',
+    writable: false
+  }
+});
 
-const person = new Person();
-person.greet();
-person.printAge();
-console.log(person)
-console.log(person.toString());
-const p2 = new person.__proto__.constructor();
-console.dir(Object.prototype.__proto__);
+const student = Object.create({
+  printProgress: function() {
+    console.log(this.progress);
+  }
+});
+
+student.name = 'Jeff';
+
+Object.defineProperty(student, 'progress', {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false
+});
+
+student.printProgress();
+
+course.printRating();
